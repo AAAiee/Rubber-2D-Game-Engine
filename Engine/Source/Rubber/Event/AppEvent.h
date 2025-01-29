0e003b5c84328c34973f3a83638497f8fafabe40
Lex Yuan <yhl20010504@gmail.com>
2025-01-29 15:42:49 -0800

    add AppEvent

--- Engine/Source/Rubber/Event/AppEvent.h
+++ Engine/Source/Rubber/Event/AppEvent.h
@@ -1,5 +1,28 @@
 #pragma once
+#include "Event.h" 
+
 namespace Rubber
 {
-	class 
+	class RB_API WindowResizeEvent : public Event
+	{
+	public:
+		WindowResizeEvent(unsigned int width, unsigned int height)
+			: m_Width(width), m_Height(height) {
+		}
+
+		inline unsigned int getWidth() const { return m_Width; }
+		inline unsigned int getHeight() const { return m_Height; }
+		
+		std::string toString() const override
+		{
+			std::stringstream ss;
+			ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+			return ss.str();
+		}
+
+		EVENT_CATEGORY(toUnderType(EventCategory::WINDOW))
+		EVENT_TYPE(EventType::WindowResized)
+	private:
+		unsigned int m_Width, m_Height;
+	};
 }
