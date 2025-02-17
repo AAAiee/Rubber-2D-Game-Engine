47b9bd3f38b97ae716cf53ec073f45d9471ca151
Lex Yuan <yhl20010504@gmail.com>
2025-02-17 00:25:39 -0800

    Added Window.h: base class for Window

---
+++ Engine/Source/Rubber/Window.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <pch.h>
+#include <Rubber/Core.h>
+#include <Rubber/Event/Event.h>
+namespace Rubber
+{
+
+	struct WindowProps
+	{
+		std::string title;
+		unsigned int width;
+		unsigned int height;
+
+		WindowProps(const std::string& title, unsigned int width, unsigned int height)
+			:title(title), width(width), height(height)
+		{
+
+		}
+
+	};
+	class RB_API Window
+	{
+	public:
+		using EventCallBackFn = std::function<void(Event&)>;
+
+		virtual ~Window() {};
+
+
+		virtual void onUpdate() = 0;
+
+		virtual unsigned int getWidth() const = 0;
+		virtual unsigned int getHeight() const = 0;
+
+		//Window Attributes 
+		virtual void setEventCallBack(const EventCallBackFn* callback) = 0;
+		virtual void setVsync(bool enabled) = 0;
+		virtual bool isVsync() const = 0;
+
+
+		//create a window depends on the different platform
+		static Window* create(const WindowProps&  = WindowProps())
+	};
+}
